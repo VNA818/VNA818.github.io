901004cbb973f5b9ada8dbf63522710f3287e6ed
Viano Arcery <33736970+VNA818@users.noreply.github.com>
2024-10-02 21:30:02 -0400

    remove src

--- publish.mjs
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { spawnSync } from "child_process";
-
-if(!process.env.SERVER_USER || !process.env.SERVER_IP) {
-    console.log("Please set SERVER_USER and SERVER_IP environment variables");
-}
-const cpyCmd = `scp -r ./dist/* ${process.env.SERVER_USER}@${process.env.SERVER_IP}:~/documents/server/web/static`;
-console.log("Executing", cpyCmd);
-spawnSync(cpyCmd, { shell: true, stdio: "inherit" })
